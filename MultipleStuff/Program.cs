d6d4f73fbe6687842db23d717fe23de28c1d8a1d
StarryNight00 <cisa.matias@gmail.com>
2019-06-14 11:27:28 +0100

    added MultipleStuff project in MyAula14. 1st oeverload working

---
+++ MultipleStuff/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultipleStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double min, max;
+
+            SuperList spList = new SuperList { 1.0, 3.5, 2.7, 60.4, 17.4, 0.8 };
+
+            spList.GetMinMax(out min, out max);
+
+            Console.WriteLine($"min is {min}, max is {max}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MultipleStuff/SuperList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleStuff
+{
+    class SuperList : List<double>
+    {
+
+        public void GetMinMax(out double min, out double max)
+        {
+            Sort();
+            min = this[0];
+            max = this[Count - 1];
+        }
+
+    }
+}
